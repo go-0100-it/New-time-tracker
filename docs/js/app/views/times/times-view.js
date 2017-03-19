624199aefa8a9edee09b5b575a8bc83892172b22
go-0100-it <davewaters@live.ca>
2017-03-19 18:59:24 -0400

    - fixed vscode error when saving all

--- docs/js/app/views/times/times-view.js
+++ docs/js/app/views/times/times-view.js
@@ -1,37 +1,26 @@
 // More description
 
-define(['jquery', 'backbone', 'underscore', 'times_model', 'times_collection', 'times_view', 'times_list_view', 'times_item_view', 'firebase_app', 'firebase_data_base'],
-    function($, backbone, _, Times, TimesCollection, TimesView, TimesListView, TimesItemView, firebaseDB, timesController) {
+define([
+        'jquery',
+        'backbone',
+        'underscore',
+        'times_model',
+        'util'
+    ],
+    function($, Backbone, _, Times, tpl) {
 
-        var createTimesView = function() {
-            require(['css!css/times/times-detail-view.css'], function() {
-                $(document).ready(function() {
-                    var timesView = new TimesView().render();
-                    renderTimesList();
-                });
-            });
-        };
-
-        var renderTimesList = function() {
-            return firebase.database().ref("times").once('value').then(function(snapshot) {
-                var times = (snapshot.val());
-
-                var data = new TimesCollection([]);
-
-                for (var time in times) {
-                    var newTime = new Times.Times(times[time]);
-                    data.add(newTime);
-                }
-
-                var timesList = new TimesListView({ model: data });
-                timesList.render();
-
-                $.hideLoading({ name: 'jump-pulse' });
-                return timesList;
-            });
-        };
-
-        return {
-            createTimesView: createTimesView
-        };
+        var TimesView = Backbone.View.extend({
+            el: '#display',
+            initialize: function() {
+                this.template = _.template(tpl.get('times-list-view'));
+            },
+            viewExists: false,
+            render: function() {
+                console.log('rendering noe times list');
+                this.$el.html(this.template());
+                this.viewExists = true;
+                return this;
+            }
+        });
+        return TimesView;
     });
